db61ea86abbb359dbb6db5e1ad6276476cfffe80
Dina <alexandra.dina40@yahoo.com>
2023-08-24 16:15:58 +0300

    declaring methods

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -10,4 +10,21 @@ public class Vehicle
     boolean damage;
     String Color;
 
+    public double accelerate(double speed,double durationInHours)
+    { // Ferrari is accelerating with 100km/h for 1h
+        System.out.println(name+ " is accelerating with "+ speed + " for " + durationInHours + " h");
+        double distance=speed*durationInHours;
+
+        totalTraveledDistance=totalTraveledDistance+distance;
+
+        System.out.println("Total distance for vehicle "+ name +":" + totalTraveledDistance);
+
+        double usedFuel=distance*mileage/100;
+        fuelLevel=fuelLevel-usedFuel;
+        System.out.println("Remaining fuel  for vehicle "+ name +":" + fuelLevel);
+
+        return distance;
+    }
+
+
 }
